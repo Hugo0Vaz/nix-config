ad183eb7f8a1ef49005f66f2a1f7c865988e55b6
Hugo Martins Vaz Silva <Hugo0Vaz>
2024-07-16 22:01:21 -0300

    update

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ figlet tree zip unzip fzf rclone fd ripgrep lazygit ];
 
-  imports = [ ./neovim.nix ./tmux.nix ];
+  imports = [ ./neovim.nix ./tmux.nix ./git.nix ];
 }
