b2b95aeac354ad8ced5ac3b66e9a280b52da9706
Hugo0Vaz <hugomartinsvaz@gmail.com>
2024-07-25 00:25:21 -0300

    132 current  2024-07-24 23:56:19  24.11.20240719.1d9c2c9  6.6.41                          *

--- modules/home-manager/scripts/default.nix
+++ modules/home-manager/scripts/default.nix
@@ -1,13 +1,14 @@
 { config, pkgs, ...}:
 
 {
-  home.file.".local/scripts/" = {
-    source = ./../../../dotfiles/scripts/.local/scripts;
-    recursive = true;
-    executable = true;
-  };
+  # home.file.".local/scripts/" = {
+  #   source = ./../../../dotfiles/scripts/.local/scripts;
+  #   recursive = true;
+  #   executable = true;
+  # };
 
   home.packages = [
     (import ./rebuildSystem.nix { inherit pkgs; })
+    pkgs.writeShellScriptBin "tmux-sessionizer" (builtins.readFile ./../../../dotfiles/scripts/.local/scripts/tmux-sessionizer.sh)
   ];
 }
