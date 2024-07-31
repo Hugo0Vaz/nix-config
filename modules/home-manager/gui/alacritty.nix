7c83b907f497d696fa213240d9bcbb45af8da07b
Hugo0Vaz <hugomartinsvaz@gmail.com>
2024-07-30 21:56:04 -0300

    138 current  2024-07-30 21:55:53  24.11.20240719.1d9c2c9  6.6.41                          *

--- modules/home-manager/gui/alacritty.nix
+++ modules/home-manager/gui/alacritty.nix
@@ -37,7 +37,6 @@
       env.TERM = "xterm-256color";
       font = {
         size = 10;
-        draw_bold_text_with_bright_colors = true;
       };
       hints = {
         alphabet = "jfkdls;ahgurieowpq";
