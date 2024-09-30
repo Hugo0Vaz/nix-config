777ea4130814e7ca7c6248b1783d7ec0d725a9c1
Hugo0Vaz <hugomartinsvaz@gmail.com>
2024-09-29 23:37:11 -0300

    145 current  2024-09-29 23:37:00  24.11.20240926.1925c60  6.6.52                          *

--- dotfiles/nvim/.config/nvim/lua/nvim.lua
+++ dotfiles/nvim/.config/nvim/lua/nvim.lua
@@ -35,6 +35,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Formatting keymap
+vim.keymap.set('n', '<leader>f', '<cmd>lua require("conform").format()<CR>', { noremap = true, silent = true })
+
 -- Terminal keymap helper
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
