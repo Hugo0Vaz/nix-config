1743e0963c467c435c9a9297d63f43c1950c173f
Hugo0Vaz <hugomartinsvaz@gmail.com>
2024-11-21 22:20:24 -0300

    157 current  2024-11-21 22:20:11  24.11.20240926.1925c60  6.6.52                          *

--- modules/home-manager/gui/hyprland.nix
+++ modules/home-manager/gui/hyprland.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ networkmanagerapplet ];
+  home.packages = with pkgs; [ networkmanagerapplet waybar ];
 
   home.file.".config/hypr/hyprland.conf" = {
     source = ./../../../dotfiles/hyprland/.config/hypr/hyprland.conf;
