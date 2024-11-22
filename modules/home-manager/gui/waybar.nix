a4fdcb33592e2e1f57b4dff694e8624ef2dd8dbe
Hugo0Vaz <hugomartinsvaz@gmail.com>
2024-11-21 22:26:06 -0300

    157 current  2024-11-21 22:20:11  24.11.20240926.1925c60  6.6.52                          *

--- modules/home-manager/gui/waybar.nix
+++ modules/home-manager/gui/waybar.nix
@@ -1,5 +1,8 @@
 { ... }: {
-  home.file.".config/waybar/config.jsonc" = {
-    source = ./../../../dotfiles/waybar/.config/waybar/config.jsonc;
+
+  home.file.".config/waybar/" = {
+    source = ./../../../dotfiles/waybar/.config/waybar;
+    recursive = true;
   };
+
 }
