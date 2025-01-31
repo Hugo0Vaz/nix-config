2a3391d9d279e8f153b2f88be1e11d3578dec2aa
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-01-30 22:04:41 -0300

    179 current  2025-01-30 22:04:26  25.05.20250108.bffc22e  6.6.69                          *

--- dotfiles/nvim/.config/nvim/lua/plugins/formatting.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,24 +1,24 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'lua-format' },
-      python = { 'autopep8' },
-      php = { 'php_cs-fixer' },
-      blade = { 'blade-formatter' },
-      nix = { 'nixfmt' },
-      javascript = { 'prettierd' },
-      html = { 'html_beautify' },
-      yaml = { 'yamlfix' },
-    },
-  },
+    'stevearc/conform.nvim',
+    opts = {
+        notify_on_error = false,
+        format_on_save = function(bufnr)
+            local disable_filetypes = {c = true, cpp = true}
+            return {
+                timeout_ms = 500,
+                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
+            }
+        end,
+        formatters_by_ft = {
+            lua = {'lua-format'},
+            python = {'autopep8'},
+            php = {'php_cs_fixer'},
+            blade = {'blade-formatter'},
+            nix = {'nixfmt'},
+            javascript = {'prettierd'},
+            html = {'html_beautify'},
+            yaml = {'yamlfix'}
+        }
+    }
 
 }
