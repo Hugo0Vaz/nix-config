721ffeb5961a88f07ba11d70149a1eb6297d8c7d
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-02-02 13:25:04 -0300

    193 current  2025-02-02 13:24:49  25.05.20250108.bffc22e  6.6.69                          *

--- dotfiles/nvim/.config/nvim/lua/plugins/which-key.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/which-key.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require('which-key').setup()
 
-        require('which-key').register {
+        require('which-key').add {
             {"<leader>c", group = "[C]ode"}, {"<leader>c_", hidden = true},
             {"<leader>d", group = "[D]ocument"}, {"<leader>d_", hidden = true},
             {"<leader>r", group = "[R]ename"}, {"<leader>r_", hidden = true},
