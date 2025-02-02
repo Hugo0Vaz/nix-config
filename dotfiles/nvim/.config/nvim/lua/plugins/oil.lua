9b311ddc4a76e15d4ffb1a465327bd75b39458a4
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-02-02 16:09:46 -0300

    199 current  2025-02-02 16:09:31  25.05.20250108.bffc22e  6.6.69                          *

--- dotfiles/nvim/.config/nvim/lua/plugins/oil.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,7 @@ return {
     opts = {},
     dependencies = {{"echasnovski/mini.icons", opts = {}}},
     config = function ()
+        require("oil").setup()
         vim.keymap.set("n", "<leader>fo", "<CMD>Oil<CR>", { desc = "Edit [F]iles in [O]il" })
     end
 }
