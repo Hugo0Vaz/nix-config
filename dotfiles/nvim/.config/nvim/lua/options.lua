02e12da016fe1a1bef0f203a1fb6ced2ad0d4b9e
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-02-02 17:36:03 -0300

    201 current  2025-02-02 17:35:46  25.05.20250108.bffc22e  6.6.69                          *

--- dotfiles/nvim/.config/nvim/lua/options.lua
+++ dotfiles/nvim/.config/nvim/lua/options.lua
@@ -34,7 +34,7 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
-vim.opt.foldnestmax = 4
+-- vim.opt.foldlevel = 99
+-- vim.opt.foldlevelstart = 1
+-- vim.opt.foldnestmax = 4
 
