7a7f0d8cdac0d75f9c928566f8d05a5f11a64073
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-04-13 20:29:54 -0300

    228 current  2025-04-13 20:29:39  25.05.20250402.2c8d3f4  6.12.21                          *

--- dotfiles/nvim/.config/nvim/lua/plugins/color_theme.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/color_theme.lua
@@ -1,15 +1,15 @@
--- return {
---   'morhetz/gruvbox',
---   priority = 1000,
---   config = function()
---     vim.cmd.colorscheme 'gruvbox'
---     vim.opt.background = 'dark'
---     vim.cmd.hi 'Comment gui=none'
---   end,
--- }
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
+  'morhetz/gruvbox',
   priority = 1000,
-  opts = {},
+  config = function()
+    vim.cmd.colorscheme 'gruvbox'
+    vim.opt.background = 'dark'
+    vim.cmd.hi 'Comment gui=none'
+  end,
 }
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+-- }
