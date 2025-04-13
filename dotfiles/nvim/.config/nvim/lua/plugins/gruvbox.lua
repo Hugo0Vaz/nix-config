466a419b0b7fea8c7294380b32211b38230bf9d4
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-04-13 20:23:12 -0300

    227 current  2025-04-13 20:22:54  25.05.20250402.2c8d3f4  6.12.21                          *

--- dotfiles/nvim/.config/nvim/lua/plugins/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'morhetz/gruvbox',
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'gruvbox'
-    vim.opt.background = 'dark'
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
