a66f2ac2c32ebd77d5f093cc57dfc78c2e370bc1
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-04-21 20:25:39 -0300

    241 current  2025-04-21 20:25:34  25.05.20250402.2c8d3f4  6.12.21                          *

--- modules/home-manager/custom/propener.nix
+++ modules/home-manager/custom/propener.nix
@@ -7,8 +7,10 @@ pkgs.buildGoModule {
     owner = "Hugo0Vaz";
     repo = "pr-opener";
     rev = "v0.1.0"; # e.g., "v1.2.0" or "a1b2c3d"
-    hash = "sha256-tFFjHqXJZA+tfvVr+K1l4hwuyhHwNS32pY4F59JD8Go=";
+    hash = "sha256-w2sJv0rjADDgBTKUU0KswrPxhAZZ1dMlJL7lQAxXTKg=";
   };
 
-  vendorHash = "sha256-+krL0+aSHNX4xbBbzNR76zzaR5Yg1cPM5h+9In8s0XQ=";
+  vendorHash = "sha256-hLT1ogwn8Y61uMXD6H2O3OsNitAE39kmCE+tOxlQDTg=";
 }
+
+
