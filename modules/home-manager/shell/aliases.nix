3921df34cbfab5f925d8c0fb16ebf0b73b2686b5
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-08-27 13:22:21 -0300

    adding support for ai

--- modules/home-manager/shell/aliases.nix
+++ modules/home-manager/shell/aliases.nix
@@ -14,6 +14,5 @@
     "aidme" = "aider --model o3-mini --api-key openai=$(pass tokens/platform.openai.com/aider)";
     "commitme" = "OPENAI_API_KEY=$(pass tokens/platform.openai.com/commiter) commiter";
     "crushme" = "ANTHROPIC_API_KEY=$(pass tokens/console.anthropic.com/crush-ai-nvim) crush";
-    "opencode" = "crush";
   };
 }
