da364f92333024109a6a3aac867103644d78ea84
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-10-05 20:37:22 -0300

    nixos: rebuild 2025-10-05 20:37

--- dotfiles/nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,7 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   cmd = { "TodoTrouble", "TodoTelescope" },
-  event = "LazyFile",
+  event = { "BufReadPre", "BufNewFile" },
   opts = {},
   -- stylua: ignore
   keys = {
