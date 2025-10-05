82d46d376510f10021fbf33e796b7eda93c657fa
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-10-05 20:44:08 -0300

    nixos: rebuild 2025-10-05 20:44

--- dotfiles/nvim/.config/nvim/lua/plugins/blame.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'FabijanZulj/blame.nvim',
-    config = function()
-        require('blame').setup()
-        vim.keymap.set('', '<leader>gb', ':BlameToggle window<CR>',
-                       {desc = 'Toggles [G]it [B]lame Window'})
-    end
-}
