6c88b178a0cb6fc359d3ea7df882039675c6d202
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-10-13 10:33:53 -0300

    nixos: rebuild 2025-10-13 10:33

--- modules/home-manager/gui/ghostty.nix
+++ modules/home-manager/gui/ghostty.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ ghostty-portable ];
+  home.packages = with pkgs; [ ghostty ];
 
   home.file.".config/ghostty/config" = {
     source = ./../../../dotfiles/ghostty/.config/ghostty/config;
--- modules/nixos/firewall.nix
+++ modules/nixos/firewall.nix
@@ -2,4 +2,6 @@
   networking.firewall.enable = true;  # Ensure the firewall is enabled
   networking.firewall.allowedTCPPorts = [ 8081 ];  # Allow TCP traffic on port 8081
   networking.firewall.allowedUDPPorts = [ 8081 ];  # Allow UDP traffic on port 8081
+  networking.firewall.allowedTCPPorts = [ 24800 ];
+  networking.firewall.allowedUDPPorts = [ 24800 ];
 }
