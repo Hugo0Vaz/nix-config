bfbccc08d2980fed7303b8435a9861c3327274b2
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-10-29 20:54:06 -0300

    nixos: rebuild 2025-10-29 20:54

--- modules/nixos/firewall.nix
+++ modules/nixos/firewall.nix
@@ -1,5 +1,5 @@
 { ... }: {
   networking.firewall.enable = true;  # Ensure the firewall is enabled
-  networking.firewall.allowedTCPPorts = [ 8081 24800 ];  # Allow TCP traffic on port 8081
-  networking.firewall.allowedUDPPorts = [ 8081 24800 ];  # Allow UDP traffic on port 8081
+  networking.firewall.allowedTCPPorts = [ 8081 24800 9090 ];  # Allow TCP traffic on port 8081
+  networking.firewall.allowedUDPPorts = [ 8081 24800 9090 ];  # Allow UDP traffic on port 8081
 }
