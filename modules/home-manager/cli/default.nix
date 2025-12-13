7b244f7b5cf012affa68021292e448f28104144f
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-12-13 12:18:58 -0300

    nixos: rebuild 2025-12-13 12:18

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -1,21 +1,4 @@
-{ pkgs, config, ... }:
-let
-  # Function that converts a relative string path to an absolute path
-  # Usage: toAbsolutePath "relative/path" or toAbsolutePath "./relative/path"
-  # Note: Must use STRING paths, not Nix path literals (no ./ prefix without quotes)
-  toAbsolutePath = relativePath: 
-    let
-      # Get the absolute path of the directory containing this file
-      thisDir = toString ./.;
-      # Remove leading ./ if present
-      cleanPath = 
-        if builtins.substring 0 2 relativePath == "./"
-        then builtins.substring 2 (builtins.stringLength relativePath) relativePath
-        else relativePath;
-    in
-      "${thisDir}/${cleanPath}";
-in
-{
+{ pkgs, config, ... }: {
   home.packages = with pkgs; [
     figlet
     tree
@@ -45,9 +28,9 @@ in
   programs.eza.enable = true;
 
 
-  # home.file."teste.txt" = { source = config.lib.file.mkOutOfStoreSymlink (toString ./teste.txt); };
-  # home.file."teste.txt" = { source = config.lib.file.mkOutOfStoreSymlink "/home/hugomvs/Projetos/nix-config/modules/home-manager/cli/teste.txt" ; };
-  home.file."teste.txt" = { source = config.lib.file.mkOutOfStoreSymlink (toAbsolutePath "./teste.txt"); };
+  home.file."teste.txt" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/home/hugomvs/Projetos/nix-config/modules/home-manager/cli/teste.txt";
+  };
 
   imports = [ ./neovim.nix ./tmux.nix ./git.nix ./ai.nix ];
 }
