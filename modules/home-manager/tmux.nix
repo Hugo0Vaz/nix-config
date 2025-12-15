09e3f4657099bea516a32cd83659f777b9b9f126
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-12-14 23:37:37 -0300

    nixos: rebuild 2025-12-14 23:37

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -3,6 +3,10 @@
     enable = true;
   };
 
+  home.packages = with pkgs; [
+    sesh
+  ];
+
   home.file.".tmux.conf" = {
     source = config.lib.file.mkOutOfStoreSymlink "${self}/modules/home-manager/tmux/.tmux.conf";
   };
