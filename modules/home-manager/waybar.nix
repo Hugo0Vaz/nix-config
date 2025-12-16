3e04bafe2f811a244244e1e0c708185cba1c1e8f
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-12-16 09:13:42 -0300

    nixos: rebuild 2025-12-16 09:13

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -1,7 +1,7 @@
 { config, self, ... }: {
 
   home.file.".config/waybar/config" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/modules/home-manager/waybar/config";
+    source = config.lib.file.mkOutOfStoreSymlink "${self}/modules/home-manager/waybar/config.jsonc";
   };
 
   home.file.".config/waybar/style.css" = {
