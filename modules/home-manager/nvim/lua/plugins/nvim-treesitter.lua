92b8cf6d5cf10dde5599f7837b016c484b2fbb99
Hugo0Vaz <hugomartinsvaz@gmail.com>
2025-12-19 14:20:52 -0300

    home-manager: generation 4 - 2025-12-19 (wsl)

--- modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
+++ modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,31 @@
-return { -- Highlight, edit, and navigate code
+return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    -- [[ Configure custom Treesitter parsers ]]
-    local parsers = require("nvim-treesitter.parsers")
 
-    -- Ensure configs table exists (required on newer versions)
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "html",
+      "lua",
+      "markdown",
+      "vim",
+      "vimdoc",
+      "php",
+      "blade",
+      "json",
+      "yaml",
+      "dockerfile",
+      "templ",
+    },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+
+  config = function(_, opts)
+    -- Custom parser MUST be defined before setup()
+    local parsers = require("nvim-treesitter.parsers")
     parsers.configs = parsers.configs or {}
 
     parsers.configs.blade = {
@@ -17,27 +37,7 @@ return { -- Highlight, edit, and navigate code
       filetype = "blade",
     }
 
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "html",
-        "lua",
-        "markdown",
-        "vim",
-        "vimdoc",
-        "php",
-        "blade",
-        "json",
-        "yaml",
-        "dockerfile",
-        "templ",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
 
