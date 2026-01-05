acdfc77b5fdc72c827badeb6581fc50bcbaccc0b
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-01-05 12:06:36 -0300

    home-manager: generation 12 - 2026-01-05 (hugom@kot225)

--- modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
+++ modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,48 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("config.treesitter")
+    require('nvim-treesitter.configs').setup({
+      -- Install parsers for these languages
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "python",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "json",
+        "markdown",
+      },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      auto_install = true,
+
+      -- Enable syntax highlighting
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+
+      -- Enable indentation
+      indent = {
+        enable = true,
+      },
+
+      -- Enable incremental selection
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+    })
   end,
 }
