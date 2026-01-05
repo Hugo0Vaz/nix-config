313d98029e7e684e799a421608a3140cd0c7b77f
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-01-04 22:40:45 -0300

    fixing some shit in treesitter

---
+++ modules/home-manager/nvim/lua/config/treesitter.lua
@@ -0,0 +1,45 @@
+-- ~/.config/nvim/lua/config/treesitter.lua
+
+require('nvim-treesitter.configs').setup({
+  -- Install parsers for these languages
+  ensure_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "python",
+    "javascript",
+    "typescript",
+    "html",
+    "css",
+    "json",
+    "markdown",
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- Enable syntax highlighting
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+
+  -- Enable indentation
+  indent = {
+    enable = true,
+  },
+
+  -- Enable incremental selection
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+})
