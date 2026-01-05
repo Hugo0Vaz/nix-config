78aa8129eaf6fb0894a6cc34a7bae429ef92d293
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-01-05 12:04:36 -0300

    home-manager: generation 11 - 2026-01-05 (hugom@kot225)

--- modules/home-manager/nvim/lua/config/treesitter.lua
+++ modules/home-manager/nvim/lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 -- ~/.config/nvim/lua/config/treesitter.lua
 
-require('nvim-treesitter').setup({
+require('nvim-treesitter.configs').setup({
   -- Install parsers for these languages
   ensure_installed = {
     "lua",
