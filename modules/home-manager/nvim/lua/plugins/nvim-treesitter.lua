ebc876061fa3b84c483e0746eaa0ad93da46a158
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-01-14 12:19:45 -0300

    nixos: rebuild 2026-01-14 12:19

--- modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
+++ modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,32 +1,100 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  lazy = false,
-  priority = 100,
-  config = function()
-    -- Install parsers for these languages
-    local install = require('nvim-treesitter').install({
-      "lua",
-      "vim",
-      "vimdoc",
-      "python",
-      "javascript",
-      "typescript",
-      "html",
-      "css",
-      "json",
-      "markdown",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  opts = {
+    -- A list of parser names, or "all"
+    ensure_installed = {
       "bash",
       "c",
-      "nix",
-      "cpp"
-    })
+      "css",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
 
-    -- Don't wait for installation to complete, let it happen in background
-    if install and install.wait then
-      vim.defer_fn(function()
-        install:wait(60000) -- wait max 1 minute
-      end, 100)
-    end
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = {},
+
+    highlight = {
+      enable = true,
+
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    },
+
+    indent = {
+      enable = true,
+    },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]f"] = "@function.outer",
+          ["]c"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]F"] = "@function.outer",
+          ["]C"] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[f"] = "@function.outer",
+          ["[c"] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[F"] = "@function.outer",
+          ["[C"] = "@class.outer",
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
