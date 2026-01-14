1ff1572cc07ab65a66dfb3a995f0dcf9ee17d4b1
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-01-14 14:54:26 -0300

    nixos: rebuild 2026-01-14 14:54

--- modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
+++ modules/home-manager/nvim/lua/plugins/nvim-treesitter.lua
@@ -37,42 +37,42 @@ return {
       "yaml",
     })
 
-    -- Setup textobjects (still uses old config API)
-    require("nvim-treesitter.configs").setup({
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            ["]C"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            ["[C"] = "@class.outer",
-          },
-        },
-      },
-    })
-
+    -- -- Setup textobjects (still uses old config API)
+    -- require("nvim-treesitter.configs").setup({
+    --   textobjects = {
+    --     select = {
+    --       enable = true,
+    --       lookahead = true,
+    --       keymaps = {
+    --         ["af"] = "@function.outer",
+    --         ["if"] = "@function.inner",
+    --         ["ac"] = "@class.outer",
+    --         ["ic"] = "@class.inner",
+    --       },
+    --     },
+    --     move = {
+    --       enable = true,
+    --       set_jumps = true,
+    --       goto_next_start = {
+    --         ["]f"] = "@function.outer",
+    --         ["]c"] = "@class.outer",
+    --       },
+    --       goto_next_end = {
+    --         ["]F"] = "@function.outer",
+    --         ["]C"] = "@class.outer",
+    --       },
+    --       goto_previous_start = {
+    --         ["[f"] = "@function.outer",
+    --         ["[c"] = "@class.outer",
+    --       },
+    --       goto_previous_end = {
+    --         ["[F"] = "@function.outer",
+    --         ["[C"] = "@class.outer",
+    --       },
+    --     },
+    --   },
+    -- })
+    --
     -- Enable treesitter features for installed languages
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
