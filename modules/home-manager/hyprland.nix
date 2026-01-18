a8f4c9e450c62a0e73690fefe78fd409818ae538
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-01-18 19:27:18 -0300

    nixos: rebuild 2026-01-18 19:27

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -15,7 +15,37 @@ with lib;
     };
   };
 
-  config = {
+  config = let
+    screenshotToPinta = pkgs.writeShellScriptBin "screenshot-to-pinta" ''
+      # Create Screenshots directory if it doesn't exist
+      mkdir -p ~/Pictures/Screenshots
+
+      # Generate filename with timestamp
+      FILENAME=~/Pictures/Screenshots/$(date +'%Y-%m-%d_%H-%M-%S.png')
+
+      # Take screenshot based on argument
+      case "$1" in
+        "area")
+          ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)" "$FILENAME"
+          ;;
+        "full")
+          ${pkgs.grim}/bin/grim "$FILENAME"
+          ;;
+        *)
+          echo "Usage: $0 {area|full}"
+          exit 1
+          ;;
+      esac
+
+      # Check if screenshot was taken successfully
+      if [ -f "$FILENAME" ]; then
+        # Open in Pinta
+        ${pkgs.pinta}/bin/pinta "$FILENAME" &
+      else
+        ${pkgs.libnotify}/bin/notify-send "Screenshot Failed" "Could not capture screenshot"
+      fi
+    '';
+  in {
     home.packages = with pkgs; [
       ydotool
       wofi
@@ -24,6 +54,9 @@ with lib;
       slurp
       wl-clipboard
       hyprpaper
+      pinta
+      libnotify
+      screenshotToPinta
     ];
 
     home.pointerCursor = {
