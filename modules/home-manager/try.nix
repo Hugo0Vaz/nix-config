1638702c76a28a8e1e190dbef346ae2fdd7b2164
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-02-02 22:16:34 -0300

    removen gambiarra do try

--- modules/home-manager/try.nix
+++ modules/home-manager/try.nix
@@ -1,31 +1,12 @@
 { pkgs, inputs, ... }:
 {
-  home.packages = with pkgs; [
-    ruby
-  ];
-
   imports = [
     inputs.try.homeModules.default
   ];
 
-  # TODO: verificar quando o pacote try tiver corrigido https://github.com/tobi/try/pull/91
   programs.try = {
     enable = true;
-    path = "~/Projetos/tryouts";
-    package = inputs.try.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
-      installPhase = ''
-        mkdir -p $out/bin $out/lib
-        cp try.rb $out/bin/try
-        cp -r lib/* $out/lib/
-        chmod +x $out/bin/try
-
-        # Update the require_relative paths to use absolute paths
-        substituteInPlace $out/bin/try \
-          --replace "require_relative 'lib/" "require_relative '$out/lib/"
-
-        wrapProgram $out/bin/try \
-          --prefix PATH : ${pkgs.ruby}/bin
-      '';
-    });
+    path = "~/Projetos/tries";  # optional, defaults to ~/src/tries
   };
+
 }
