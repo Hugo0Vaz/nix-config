27bda3f5447e754f2fb59ec93046c315e7e1249d
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-03-27 17:48:11 -0300

    refactor

---
+++ modules/aspects/office.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.office =
+    { pkgs }:
+    {
+      environment.systemPackages =
+        with pkgs;
+        [
+          teams-for-linux
+          libreoffice
+          obsidian
+          calibre
+          kdePackages.okular
+          speedcrunch
+        ];
+    };
+}
