e55cf912c5eaf8f8062f0ce406ba1c5a89af57bd
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-04-06 20:29:36 -0300

    (feat): adding desktop applications

---
+++ modules/aspects/desktop.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.desktop =
+    { pkgs, ... }:
+    {
+      environment.systemPackages =
+        with pkgs;
+        [
+          nautilus
+          localsend
+          inkscape
+          gimp2
+          pinta
+          copyq
+        ];
+    };
+}
--- modules/hosts/nixos-notebook/configuration.nix
+++ modules/hosts/nixos-notebook/configuration.nix
@@ -7,6 +7,7 @@
         inputs.self.modules.nixos.audio
         inputs.self.modules.nixos.browsers
         inputs.self.modules.nixos.cli-tools
+        inputs.self.modules.nixos.desktop
         inputs.self.modules.nixos.hugo
         inputs.self.modules.nixos.local-time
         inputs.self.modules.nixos.niri
