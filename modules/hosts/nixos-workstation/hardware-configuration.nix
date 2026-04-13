366fc9bd4dae07e586cbbcb033d51fc4167750e8
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-04-13 10:48:06 -0300

    (feat): importing hardware configuration from nixos-workstation

--- modules/hosts/nixos-workstation/hardware-configuration.nix
+++ modules/hosts/nixos-workstation/hardware-configuration.nix
@@ -1,32 +1,32 @@
 {
   flake.modules.nixos.nixosWorkstationHardwareConfiguration =
-    { config, lib, pkgs, modulesPath, ... }:
-    {
-      imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  { config, lib, pkgs, modulesPath, ... }:
+  {
+    imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-      boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
-      boot.initrd.kernelModules = [ ];
-      boot.kernelModules = [ "kvm-intel" ];
-      boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules =
+      [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-amd" ];
+    boot.extraModulePackages = [ ];
 
-      fileSystems."/" = {
-        device = "/dev/disk/by-uuid/8f966aaa-64c8-4ced-99d2-0c4ea77dad48";
-        fsType = "ext4";
-      };
-
-      fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/C537-E880";
-        fsType = "vfat";
-        options = [ "fmask=0077" "dmask=0077" ];
-      };
-
-      swapDevices = [
-        { device = "/dev/disk/by-uuid/4fb599d4-2382-4e2c-8887-61dd85e0fff2"; }
-      ];
-
-      networking.useDHCP = lib.mkDefault true;
-
-      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/4edbcdc5-e71e-49a9-9cf1-bb98d4b35814";
+      fsType = "ext4";
     };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/D7AA-DF50";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+    swapDevices = [ ];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode =
+      lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
