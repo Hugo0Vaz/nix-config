448bf2d14b53ae049dadb4d9095513a2c5fcfc90
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-04-13 10:40:56 -0300

    (feat): adding nixos-workstation host

---
+++ modules/hosts/nixos-workstation/configuration.nix
@@ -0,0 +1,34 @@
+{
+  flake.modules.nixos.nixosWorkstationConfiguration =
+    { inputs, pkgs, ... }: {
+      imports = [
+        inputs.self.modules.nixos.nixosWorkstationHardwareConfiguration
+        inputs.self.modules.nixos.abnt2
+        inputs.self.modules.nixos.admin
+        inputs.self.modules.nixos.audio
+        inputs.self.modules.nixos.browsers
+        inputs.self.modules.nixos.cli-tools
+        inputs.self.modules.nixos.desktop
+        inputs.self.modules.nixos.hugo
+        inputs.self.modules.nixos.local-time
+        inputs.self.modules.nixos.niri
+        inputs.self.modules.nixos.nvim
+        inputs.self.modules.nixos.office
+        inputs.self.modules.nixos.openssh
+        inputs.self.modules.nixos.ssh-authorized-keys
+        inputs.self.modules.nixos.podman
+        inputs.self.modules.nixos.shell
+        inputs.self.modules.nixos.sops
+        inputs.self.modules.nixos.starship
+        inputs.self.modules.nixos.systemd-boot
+        inputs.self.modules.nixos.tailscale
+        inputs.self.modules.nixos.terminals
+        inputs.self.modules.nixos.tmux
+      ];
+
+      networking.hostName = "nixos-workstation";
+      system.stateVersion = "25.11";
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      nixpkgs.config.allowUnfree = true;
+    };
+}
