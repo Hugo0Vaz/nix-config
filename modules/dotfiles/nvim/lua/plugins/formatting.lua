054b11963dd5fd47142133569b5f969ab268b6af
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-04-26 14:25:37 -0300

    (feat): melhorando a formatação do nvim

--- modules/dotfiles/nvim/lua/plugins/formatting.lua
+++ modules/dotfiles/nvim/lua/plugins/formatting.lua
@@ -1,32 +1,38 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-            local disable_filetypes = {c = true, cpp = true}
-            return {
-                timeout_ms = 500,
-                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
-            }
-        end,
-        formatters_by_ft = {
-            lua = {'lua-format'},
-            python = {'autopep8'},
-            php = {'php_cs_fixer'},
-            blade = {'blade-formatter'},
-            nix = {'nixpkgs_fmt'},
-            javascript = {'prettierd'},
-            html = {'html_beautify'},
-            yaml = {'yamlfix'}
-        }
+  'stevearc/conform.nvim',
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return
+      end
+      return {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      python = { 'autopep8' },
+      php = { 'php_cs_fixer' },
+      blade = { 'blade-formatter' },
+      nix = { 'nixpkgs_fmt' },
+      javascript = { 'prettierd' },
+      html = { 'html_beautify' },
+      yaml = { 'yamlfix' },
     },
-    config = function()
-        -- Formatting keymap with action logging
-        vim.keymap.set('n', '<leader>bf',
-                       function()
-                           print("Formatting buffer...")
-                           require("conform").format()
-                       end,
-                       {noremap = true, silent = false, desc = "[B]uffer [F]ormat"})
-    end
+  },
+  keys = {
+    {
+      '<leader>bf',
+      function()
+        print 'Formatting buffer...'
+        require('conform').format()
+      end,
+      noremap = true,
+      silent = false,
+      desc = '[B]uffer [F]ormat',
+    },
+  },
 }
