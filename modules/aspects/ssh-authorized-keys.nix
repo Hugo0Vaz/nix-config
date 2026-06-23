9c7cf2e2b422dd52c42147c077d950b5893ffacc
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-06-23 12:30:03 -0300

    feat(ssh): updating ssh keys

--- modules/aspects/ssh-authorized-keys.nix
+++ modules/aspects/ssh-authorized-keys.nix
@@ -5,9 +5,9 @@
       options.my.ssh.authorizedKeys = lib.mkOption {
         type = lib.types.listOf lib.types.str;
         default = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBUinTmOEky+U/j8Dh5tUhsyWxnMgkpGsKH3uQKKGNgN hugom@kot225"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6yWX3IBaW8JOj/NBygnPOrIqrFMcoDuAQ1oGYKueUa hugomvs@nixos-workstation"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQNQIRKgJxqQjk9HyJl5hWiQmOc0QRhDWNUARZ8CLF3 hugomvs@nixos-workstation"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICodiRq/5jSy1HfpNohFqF0TYmYxMYexx8+6gs9wtqlI hugomvs@nixos-kot225"
         ];
         description = "SSH authorized_keys shared across hosts.";
       };
