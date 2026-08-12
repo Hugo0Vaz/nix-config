455dd6107d4fe21c4e8ebf0158d6aed142a3b06f
Hugo0Vaz <hugomartinsvaz@gmail.com>
2026-08-11 21:18:02 -0300

    feat(snippets): yasnippets

--- modules/dotfiles/emacs/init.el
+++ modules/dotfiles/emacs/init.el
@@ -93,6 +93,8 @@
 :init
 (yas-global-mode 1))
 
+(use-package yasnippet-snippets)
+
 (use-package markdown-mode
 :mode
 ("\.md\'" . markdown-mode)
@@ -213,3 +215,18 @@ ugo/org-capture-logbook-find-today)
 
 (put 'erase-buffer 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company counsel-projectile doom-modeline gruvbox-theme magit
+	     markdown-mode rainbow-delimiters yasnippet
+	     yasnippet-snippets)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
